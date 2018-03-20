6a1bc9f494a36e5826b7fdb836e776eb87019d56
munenelewis <munene2379@gmail.com>
2018-03-20 15:27:22 +0400

    removed cached

--- Hood/settings.py
+++ Hood/settings.py
@@ -82,8 +82,10 @@ WSGI_APPLICATION = 'Hood.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'hood',
+        'USER': 'anynonimus',
+    'PASSWORD':'anynonimus',
     }
 }
 
